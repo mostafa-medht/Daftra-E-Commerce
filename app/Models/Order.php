d806bbb9bcac93f24cd267ca538a29479158d99b
Mostafa Med7t <mostafamedht98@gmail.com>
2024-10-19 19:36:48 +0300

    add middle table name between relations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,6 +11,6 @@ class Order extends Model
 
     public function products(): BelongsToMany
     {
-        return $this->belongsToMany(Product::class, 'order_products')->withPivot('quantity');
+        return $this->belongsToMany(Product::class, 'order_product')->withPivot('quantity');
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,6 +19,6 @@ class Product extends Model
 
     public function orders(): BelongsToMany
     {
-        return $this->belongsToMany(Order::class)->withPivot('quantity');
+        return $this->belongsToMany(Order::class, 'order_product')->withPivot('quantity');
     }
 }
