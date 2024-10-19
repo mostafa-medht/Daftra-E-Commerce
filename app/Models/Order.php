a227f5c5d2fac5af22170c5b4cc35c06784a6fe0
Mostafa Med7t <mostafamedht98@gmail.com>
2024-10-19 11:52:23 +0300

    add model realtions & migration

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $fillable = ['customer_name', 'customer_email'];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_products')->withPivot('quantity');
+    }
 }
